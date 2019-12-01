ad28c0e3519405b3986f0fd9f25e3a6b3782ce92
chrustkiran <christkiran.15@cse.mrt.ac.lk>
2019-12-01 18:59:46 +0530

    Updates

---
+++ precache-manifest.7f8854587c9b08f1a138a95c1cac2252.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a20b6d6028a4161590d9fc708efac3cb",
+    "url": "/olichuvadu/index.html"
+  },
+  {
+    "revision": "da5e2c991b497d450c05",
+    "url": "/olichuvadu/static/css/2.90172c5a.chunk.css"
+  },
+  {
+    "revision": "95a6b749a515b9d5db94",
+    "url": "/olichuvadu/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "da5e2c991b497d450c05",
+    "url": "/olichuvadu/static/js/2.9f5579be.chunk.js"
+  },
+  {
+    "revision": "95a6b749a515b9d5db94",
+    "url": "/olichuvadu/static/js/main.7295dab4.chunk.js"
+  },
+  {
+    "revision": "fc6028749dcb96cbdb1a",
+    "url": "/olichuvadu/static/js/runtime-main.c0c2bda5.js"
+  }
+]);
